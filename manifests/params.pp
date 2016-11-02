39de91d0625ce40be20905d1caf8e85c5e00a413
ShadowZero3000 <josh.souza@greenhouse.io>
2016-11-02 11:53:23 -0400

    Adding OSX Home dir

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,9 @@
 class ohmyzsh::params {
   case $::operatingsystem {
+    Darwin: {
+      $zsh = '/usr/bin/zsh',
+      $home = '/Users'
+    }
     default: {
       $zsh = '/usr/bin/zsh'
       $home = '/home'
